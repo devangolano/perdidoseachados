1769bf564d61f9f0f8568bb1160cce36a135a1dc
Dev Angolano <albertoronny237@gmail.com>
2024-06-17 00:16:32 +0100

    inicio do projecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar"
+import Card from "./components/Card"
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+   <div className="mt-12 grid gap-2 grid-cols-1 md:grid-cols-3 px-8">
+   <Card />
+ 
+
+   </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-white text-2xl">Teste de Navbar</h1>
+        <ul className="flex space-x-4">
+          <li><a href="#" className="text-white hover:text-gray-400">Home</a></li>
+          <li><a href="#" className="text-white hover:text-gray-400">About</a></li>
+          <li><a href="#" className="text-white hover:text-gray-400">Services</a></li>
+          <li><a href="#" className="text-white hover:text-gray-400">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
