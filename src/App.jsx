708936fc6e7a1e838ddeaa04d2ac9e29e1a7fa6c
Dev Angolano <albertoronny237@gmail.com>
2024-06-17 01:33:33 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
     <Navbar />
-   <div className="mt-12 grid gap-y-10 gap-x-3 grid-cols-1 md:grid-cols-3 px-6">
+   <div className="mt-12 grid gap-y-10 gap-x-3 grid-cols-1 md:grid-cols-3 px-7">
    <Card />
    <Card />
    <Card />
