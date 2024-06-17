5e41d64c32c74ad043bed15a540d32198a1e1615
Dev Angolano <albertoronny237@gmail.com>
2024-06-17 01:41:41 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import Navbar from "./components/Navbar"
-import Card from "./components/Card"
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
 
 function App() {
-
   return (
     <>
-    <Navbar />
-   <div className="mt-12 grid gap-y-10 gap-x-3 grid-cols-1 md:grid-cols-3 px-7">
-   <Card />
-   <Card />
-   <Card />
-   <Card />
-   <Card />
-   <Card />
-   </div>
+      <Navbar />
+      <div className="mt-12 max-w-screen-xl mx-auto px-6">
+        <div className="grid gap-y-10 gap-x-4 grid-cols-1 md:grid-cols-4 justify-items-center">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
