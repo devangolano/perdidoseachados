5ec7c08f55a66bbd21d561b733e719d7e19e6c1d
Dev Angolano <albertoronny237@gmail.com>
2024-07-05 16:18:26 +0100

    fix

--- src/components/Carrocel.jsx
+++ src/components/Carrocel.jsx
@@ -3,7 +3,7 @@ import { Carousel, Typography } from "@material-tailwind/react";
 
 export function Carrocel() {
   return (
-    <Carousel className="rounded-xl">
+    <Carousel className="">
       <div className="relative h-96 w-full">
         <img
           src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
