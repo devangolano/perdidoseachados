f20792ec6f3a2ef77f1d4621e8749f06c894504e
Dev Angolano <albertoronny237@gmail.com>
2024-07-18 02:02:32 +0100

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,3 @@
-import React, { useState } from 'react';
-import { FaSearch } from "react-icons/fa";
-
 
 const Navbar = () => {
   return (
@@ -9,13 +6,13 @@ const Navbar = () => {
         <img
           src="https://t3.ftcdn.net/jpg/05/09/17/46/360_F_509174694_a8jxmbhzeDgJOu0VYNQwDZC61xjZWCtJ.jpg"
           alt="Logo"
-          className="h-10"
+          className="h-10 md:h-12"
         />
         <div className="hidden md:flex ml-10 space-x-4">
-          <a href="#" className="text-gray-800 hover:text-purple-600">Home</a>
-          <a href="#" className="text-gray-800 hover:text-purple-600">Services</a>
-          <a href="#" className="text-gray-800 hover:text-purple-600">Contact us</a>
-          <a href="#" className="text-gray-800 hover:text-purple-600">Our projects</a>
+          <a href="#" className="text-gray-800 hover:text-purple-600">Principal</a>
+          <a href="#" className="text-gray-800 hover:text-purple-600">Perdidos</a>
+          <a href="#" className="text-gray-800 hover:text-purple-600">Encontrados</a>
+          <a href="#" className="text-gray-800 hover:text-purple-600">Contactos</a>
         </div>
         <div className="hidden md:flex items-center space-x-4">
           <button className="text-gray-800 hover:text-purple-600">
@@ -28,12 +25,12 @@ const Navbar = () => {
             <img
               src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
               alt="Profile"
-              className="h-8 w-8 rounded-full"
+              className="h-10 w-10 rounded-full"
             />
             <span className="absolute bottom-0 right-0 block h-2 w-2 rounded-full ring-2 ring-white bg-green-400"></span>
           </div>
           <button className="bg-purple-600 text-white px-4 py-2 rounded-full hover:bg-purple-700">
-            Create Story
+            Cadastrar Doc
           </button>
         </div>
         <div className="md:hidden flex items-end">
